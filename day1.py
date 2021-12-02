95d8e3eefbdfbe71c8c146c3141b736525d5a82e
Zack Ng <ngzhicai98@gmail.com>
2021-12-01 16:24:58 -0800

    Refactor day1

--- day1.py
+++ day1.py
@@ -1,3 +1,43 @@
+def count_increases_part1(tests: list[int]) -> int:
+    # account for edge case of missing tests
+    if not len(tests):
+        return 0
+
+    no_increases = 0
+    # implicit assumption: tests not empty
+    prev = tests[0]
+    for case in tests[1:]:
+        if case > prev:
+            no_increases += 1
+        prev = case
+
+    return no_increases
+
+
+def count_increases_part2(tests: list[int]) -> int:
+    no_window_increases = 0
+
+    left = 0
+    curr_sum = 0
+    prev_sum = None
+
+    for right in range(len(tests)):
+        curr_sum += tests[right]
+
+        while right - left + 1 > 3:
+            curr_sum -= tests[left]
+            left += 1
+
+        if right - left + 1 == 3 and prev_sum != None:
+            if curr_sum > prev_sum:
+                no_window_increases += 1
+            prev_sum = curr_sum
+        elif right - left + 1 == 3:
+            prev_sum = curr_sum
+
+    return no_window_increases
+
+
 tests = [
     178,
     205,
@@ -2000,47 +2040,6 @@ tests = [
     8753,
     8755,
 ]
-
-
-def count_increases_part1(tests: list[int]) -> int:
-    if not len(tests):
-        return 0
-
-    no_increases = 0
-    prev = tests[0]
-    for case in tests[1:]:
-        if case > prev:
-            no_increases += 1
-        prev = case
-
-    return no_increases
-
-
-def count_increases_part2(tests: list[int]) -> int:
-    no_window_increases = 0
-
-    left = 0
-    curr_sum = 0
-    prev_sum = None
-
-    for right in range(len(tests)):
-        curr_sum += tests[right]
-
-        while right - left + 1 > 3:
-            curr_sum -= tests[left]
-            left += 1
-
-        print(f'for range {left} to {right}, curr_sum is {curr_sum}')
-
-        if right - left + 1 == 3 and prev_sum != None:
-            if curr_sum > prev_sum:
-                no_window_increases += 1
-            prev_sum = curr_sum
-        elif right - left + 1 == 3:
-            prev_sum = curr_sum
-
-    return no_window_increases
-
 
 if __name__ == '__main__':
     print(count_increases_part1(tests))
